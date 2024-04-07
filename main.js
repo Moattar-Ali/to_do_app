11ca89c07ae32c8c9310282b123001786ed131ac
Moattar Ali <164565363+Moattar-Ali@users.noreply.github.com>
2024-04-07 23:08:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let to_dos = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([{
+            name: "todo",
+            type: "input",
+            message: "What do you want to add in your to_dos?",
+        },
+        {
+            name: "add_more",
+            message: "Would you like to add more in your to dos?",
+            type: "confirm",
+            default: "true",
+        }]);
+    to_dos.push(addTask.todo);
+    // condition = addTask.addMore;
+    console.log(to_dos);
+    condition = addTask.addMore;
+}
